6d889d8c5955f231412054f1aed489623878eb4c
arnavakula <akula.arnav@gmail.com>
2020-06-18 16:30:50 -0700

    Made sign up screen

--- main.py
+++ main.py
@@ -5,12 +5,19 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 Builder.load_file("design.kv")
  
 class LoginScreen(Screen):
-    pass
+    def sign_up(self):
+        self.manager.current = "sign_up_screen"
+
+class SignUpScreen(Screen):
+    def login(self):
+        self.manager.current = "login_screen"
+
 class RootWidget(ScreenManager):
     pass
+
 class MainApp(App):
     def build(self):
         return RootWidget()
  
-if __name__=="__main__":
+if __name__== "__main__":
     MainApp().run()
