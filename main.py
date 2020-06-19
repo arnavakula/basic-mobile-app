9640bf3c2977044c0ef5255fcf97798f8f79f6e1
arnavakula <akula.arnav@gmail.com>
2020-06-18 18:31:48 -0700

    Implemented sign-up functionality

--- main.py
+++ main.py
@@ -1,17 +1,38 @@
+from datetime import datetime
+import json
+
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.uix.screenmanager import ScreenManager, Screen
- 
-Builder.load_file("design.kv")
+from kivy.uix.screenmanager import Screen, ScreenManager
+
+Builder.load_file('design.kv')
  
 class LoginScreen(Screen):
     def sign_up(self):
-        self.manager.current = "sign_up_screen"
+        self.manager.current = 'sign_up_screen'
+
 
 class SignUpScreen(Screen):
     def login(self):
-        self.manager.current = "login_screen"
+        self.manager.transition.direction = 'right'
+        self.manager.current = 'login_screen'
+    def add_user(self, name, pword):
+        with open('users.json') as f:
+            users = json.load(f)   
+        
+        users[name] = {'username': name, 'password': pword, 'created': datetime.now().strftime('%m-%d-%Y')}  
 
+        with open('users.json', 'w') as f:
+            json.dump(users, f)
+
+        self.manager.current = 'sign_up_success_screen'
+
+class SignUpSuccessScreen(Screen):
+    def login(self):
+        self.manager.transition.direction = 'right'
+        self.manager.current = 'login_screen'
+    pass
+        
 class RootWidget(ScreenManager):
     pass
 
@@ -19,5 +40,5 @@ class MainApp(App):
     def build(self):
         return RootWidget()
  
-if __name__== "__main__":
+if __name__== '__main__':
     MainApp().run()
